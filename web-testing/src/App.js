a11c5b0c6349c5de1c0ca50ff4e62d2c15491cf3
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-03 16:18:33 -0400

    Set up buttons to change ball, strike, and foul state in dashboard.js

--- web-testing/src/App.js
+++ web-testing/src/App.js
@@ -5,15 +5,15 @@ import Dashboard from './components/Dashboard.js'
 
 function App() {
 
-  const [ball, setBall] = useState()
-  const [strike, setStrike] = useState()
-  const [foul, setFoul] = useState()
+  const [ball, setBall] = useState(0)
+  const [strike, setStrike] = useState(0)
+  const [foul, setFoul] = useState(0)
 
   return (
     <div className="App">
       <h1>Scoreboard</h1>
-      <Display />
-      <Dashboard />
+      <Display  ball={ball} strike={strike}/>
+      <Dashboard ball={ball} setBall={setBall} strike={strike} setStrike={setStrike} foul={foul} setFoul={setFoul} />
     </div>
   );
 }
--- web-testing/src/components/Dashboard.js
+++ web-testing/src/components/Dashboard.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
 
-function Dashboard() {
+function Dashboard(props) {
 
+
+    const strikeCounter = () => {
+        props.setStrike(props.strike + 1)
+    }
+
+    const ballCounter = () => {
+
+        props.setBall(props.ball + 1)
+    }
+
+    const foulCounter = () => {
+        props.setFoul(props.foul + 1)
+    }
 
     return (
         <div>
             Dashboard
+
+            <button onClick={() => strikeCounter()}> Strike</button>
+
+            <button onClick={() => ballCounter()}>Ball</button>
+
+            <button onClick={() => foulCounter()}>Foul</button>
+
         </div>
 
 
