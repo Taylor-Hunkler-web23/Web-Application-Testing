760cd7cf20c5db9f2c8c01e03f0d316ca9e98428
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-03 15:48:28 -0400

    Set up state for ball, strike, and foul in app.js

--- web-testing/src/App.js
+++ web-testing/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Display from './components/Display.js'
+import Dashboard from './components/Dashboard.js'
 
 function App() {
+
+  const [ball, setBall] = useState()
+  const [strike, setStrike] = useState()
+  const [foul, setFoul] = useState()
+
   return (
     <div className="App">
-<h1>Scoreboard</h1>
+      <h1>Scoreboard</h1>
+      <Display />
+      <Dashboard />
     </div>
   );
 }
---
+++ web-testing/src/components/Display.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Display() {
+
+
+
+    return (
+
+        <div>
+            <h1>Balls</h1>
+            <h1>Strikes</h1>
+
+
+        </div>
+    )
+}
+
+export default Display;
